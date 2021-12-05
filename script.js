42e885b64cd9d927c270e22e5f91ff99f312601d
Cup of Coding <anne@cupofcoding.com>
2021-12-04 16:00:18 -0800

    added new icon pngs and code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.getElementById('menu-button')
 const nav = document.getElementById('menu')
+let $toggler = document.getElementById('toggler'),
 
 function navToggle() {
     button.classList.toggle('open')
@@ -8,3 +9,23 @@ function navToggle() {
 }
 
 button.addEventListener('click', navToggle)
+
+if ($calculator.classList.contains('dark')) {
+    $toggler.querySelector('#light').style.display = 'block';
+    $toggler.querySelector('#dark').style.display = 'none';
+} else {
+    $toggler.querySelector('#light').style.display = 'none';
+    $toggler.querySelector('#dark').style.display = 'block';
+}
+
+$toggler.addEventListener('click', function () {
+    $calculator.classList.toggle('dark');
+
+    if ($calculator.classList.contains('dark')) {
+        $toggler.querySelector('#light').style.display = 'block';
+        $toggler.querySelector('#dark').style.display = 'none';
+    } else {
+        $toggler.querySelector('#light').style.display = 'none';
+        $toggler.querySelector('#dark').style.display = 'block';
+    }
+})
