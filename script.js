2d11eaa7a7524ff975b01124f87da851382736f7
Cup of Coding <anne@cupofcoding.com>
2022-09-26 14:03:26 -0700

    temp display changes

--- script.js
+++ script.js
@@ -147,3 +147,25 @@ function loadActive() {
 //     document.getElementById("img01").src = element.src;
 //     document.getElementById("modal01").style.display = "block";
 //   }
+
+// Carousel
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+    interval: false
+  });
+  
+  // for every slide in carousel, copy the next slide's item in the slide.
+  // Do the same for the next, next item.
+  $('.multi-item-carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
